e62f8c33e8e42efceed3cc0a08b5433955d5813f
viviblasco <viviblasco@hotmail.com>
2020-10-27 17:37:29 -0300

    se subio la parte de socios

--- client/js/main.js
+++ client/js/main.js
@@ -1,42 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.tooltips-general').tooltip('hide');
-    $('.mobile-menu-button').on('click', function(){
-        var mobileMenu=$('.navbar-lateral');	
-        if(mobileMenu.css('display')=='none'){
+    $('.mobile-menu-button').on('click', function() {
+        var mobileMenu = $('.navbar-lateral');
+        if (mobileMenu.css('display') == 'none') {
             mobileMenu.fadeIn(300);
-        }else{
+        } else {
             mobileMenu.fadeOut(300);
         }
     });
-    $('.desktop-menu-button').on('click', function(e){
+    $('.desktop-menu-button').on('click', function(e) {
         e.preventDefault();
-        var NavLateral=$('.navbar-lateral'); 
-        var ContentPage=$('.content-page-container');   
-        if(NavLateral.hasClass('desktopMenu')){
+        var NavLateral = $('.navbar-lateral');
+        var ContentPage = $('.content-page-container');
+        if (NavLateral.hasClass('desktopMenu')) {
             NavLateral.removeClass('desktopMenu');
             ContentPage.removeClass('desktopMenu');
-        }else{
+        } else {
             NavLateral.addClass('desktopMenu');
             ContentPage.addClass('desktopMenu');
         }
     });
-    $('.dropdown-menu-button').on('click', function(e){
+    $('.dropdown-menu-button').on('click', function(e) {
         e.preventDefault();
-        var icon=$(this).children('.icon-sub-menu');
-        if(icon.hasClass('zmdi-chevron-down')){
+        var icon = $(this).children('.icon-sub-menu');
+        if (icon.hasClass('zmdi-chevron-down')) {
             icon.removeClass('zmdi-chevron-down').addClass('zmdi-chevron-up');
             $(this).addClass('dropdown-menu-button-active');
-        }else{
+        } else {
             icon.removeClass('zmdi-chevron-up').addClass('zmdi-chevron-down');
             $(this).removeClass('dropdown-menu-button-active');
         }
-        
-        var dropMenu=$(this).next('ul');
+
+        var dropMenu = $(this).next('ul');
         dropMenu.slideToggle('slow');
     });
-    $('.exit-system-button').on('click', function(e){
+    $('.exit-system-button').on('click', function(e) {
         e.preventDefault();
-        var LinkExitSystem=$(this).attr("data-href");
+        var LinkExitSystem = $(this).attr("data-href");
         swal({
             title: "¿Estás seguro?",
             text: "Quieres salir del sistema y cerrar la sesión actual",
@@ -46,55 +46,56 @@ $(document).ready(function(){
             confirmButtonText: "Si, salir",
             cancelButtonText: "No, cancelar",
             animation: "slide-from-top",
-            closeOnConfirm: false 
-        },function(){
-            window.location=LinkExitSystem; 
-        });  
+            closeOnConfirm: false
+        }, function() {
+            window.location = LinkExitSystem;
+        });
     });
-    $('.search-book-button').click(function(e){
+    $('.search-book-button').click(function(e) {
         e.preventDefault();
-        var LinkSearchBook=$(this).attr("data-href");
+        var LinkSearchBook = $(this).attr("data-href");
         swal({
-           title: "¿Qué libro estás buscando?",
-           text: "Por favor escribe el nombre del libro",
-           type: "input",   
-           showCancelButton: true,
-           closeOnConfirm: false,
-           animation: "slide-from-top",
-           cancelButtonText: "Cancelar",
-           confirmButtonText: "Buscar",
-           confirmButtonColor: "#3598D9",
-           inputPlaceholder: "Escribe aquí el nombre de libro" }, 
-      function(inputValue){
-           if (inputValue === false) return false;  
+                title: "¿Qué audiolibro estás buscando?",
+                text: "Por favor escribe el nombre del audiolibro",
+                type: "input",
+                showCancelButton: true,
+                closeOnConfirm: false,
+                animation: "slide-from-top",
+                cancelButtonText: "Cancelar",
+                confirmButtonText: "Buscar",
+                confirmButtonColor: "#3598D9",
+                inputPlaceholder: "Escribe aquí el nombre del audiolibro"
+            },
+            function(inputValue) {
+                if (inputValue === false) return false;
 
-           if (inputValue === "") {
-               swal.showInputError("Debes escribir el nombre del libro");     
-               return false;   
-           } 
-            window.location=LinkSearchBook+"?bookName="+inputValue;
-       });
+                if (inputValue === "") {
+                    swal.showInputError("Debes escribir el nombre del audiolibro");
+                    return false;
+                }
+                window.location = LinkSearchBook + "?bookName=" + inputValue;
+            });
     });
-    $('.btn-help').on('click', function(){
+    $('.btn-help').on('click', function() {
         $('#ModalHelp').modal({
             show: true,
             backdrop: "static"
         });
     });
 });
-(function($){
-    $(window).load(function(){
+(function($) {
+    $(window).load(function() {
         $(".nav-lateral-scroll").mCustomScrollbar({
-            theme:"light-thin",
+            theme: "light-thin",
             scrollbarPosition: "inside",
             autoHideScrollbar: true,
-            scrollButtons:{ enable: true }
+            scrollButtons: { enable: true }
         });
         $(".custom-scroll-containers").mCustomScrollbar({
-            theme:"dark-thin",
+            theme: "dark-thin",
             scrollbarPosition: "inside",
             autoHideScrollbar: true,
-            scrollButtons:{ enable: true }
+            scrollButtons: { enable: true }
         });
     });
 })(jQuery);
